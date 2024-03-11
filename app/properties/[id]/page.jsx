d8385522d137dd8a5695c2142aa8279f98e98e09
Bikash <bikash_limvu@yahoo.com>
2024-03-11 16:59:51 +0000

    Worked on properties pages

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+const PropertyPage = () => {
+  return <div>PropertyPage</div>;
+};
+export default PropertyPage;
+
+// import {
+//   useRouter,
+//   useParams,
+//   useSearchParams,
+//   usePathname,
+// } from 'next/navigation';
+
+// const Property = () => {
+//   const router = useRouter();
+//   const { id } = useParams();
+//   const searchParams = useSearchParams();
+//   const name = searchParams.get('name');
+//   const pathname = usePathname();
+
+//   return (
+//     <div>
+//       <button onClick={() => router.push('/')} className="bg-blue-500 p-2">
+//         Go Home
+//       </button>
+//     </div>
+//   );
+// };
+// export default Property;
