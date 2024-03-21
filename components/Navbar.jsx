c2fdd7ad455d15553bb84ee2c36c1334c066e315
Bikash <bikash_limvu@yahoo.com>
2024-03-21 16:08:44 +0000

    Added middleware for  route restrictions

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -191,6 +191,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -200,6 +203,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
