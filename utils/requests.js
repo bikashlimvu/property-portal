2f3c13b8a05fabc251bef3fd526cf0ded12c1ac1
Bikash <bikash_limvu@yahoo.com>
2024-03-24 14:59:55 +0000

    Setup Cloudinary to host property images

--- utils/requests.js
+++ utils/requests.js
@@ -9,7 +9,7 @@ async function fetchProperties() {
       return [];
     }
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    const res = await fetch(`${apiDomain}/properties`, { cache: 'no-store' });
 
     if (!res.ok) {
       throw new Error('Failed to fetch data');
