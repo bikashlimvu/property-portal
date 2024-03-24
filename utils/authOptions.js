1412a3494562404df15b7920aa9cb9440fe8fe3c
Bikash <bikash_limvu@yahoo.com>
2024-03-24 16:04:02 +0000

    Updated property images links with Cloudinary asset urls

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -7,7 +7,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizations: {
+      authorization: {
         params: {
           prompt: 'consent',
           access_type: 'offline',
