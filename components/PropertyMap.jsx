89a92cb2c9a7767acd62d09a1beca3a483c1895b
Bikash <bikash_limvu@yahoo.com>
2024-04-16 21:15:46 +0100

    Added maps for property location on the details page

---
+++ components/PropertyMap.jsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { setDefaults, fromAddress } from 'react-geocode';
+import Spinner from './Spinner';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+const PropertyMap = ({ property }) => {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+
+  const containerStyle = {
+    width: '100%',
+    height: '500px',
+  };
+
+  const [center, setCenter] = useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  setDefaults({
+    key: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+    language: 'en',
+    region: 'us',
+  });
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  });
+
+  useEffect(() => {
+    const fetchCoords = async () => {
+      const res = await fromAddress(
+        `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+      );
+
+      const { lat, lng } = res.results[0].geometry.location;
+
+      setLat(lat);
+      setLng(lng);
+
+      setCenter({
+        lat,
+        lng,
+      });
+
+      setLoading(false);
+    };
+
+    fetchCoords();
+  }, []);
+
+  if (loading && isLoaded) return <Spinner loading={loading} />;
+
+  return (
+    !loading && (
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <Marker position={center} />
+      </GoogleMap>
+    )
+  );
+};
+export default PropertyMap;
