133f9992233da8efeb9941f79ece4cd3a1f64be2
Bikash <bikash_limvu@yahoo.com>
2024-05-22 13:19:53 +0100

    Added pagination feature on properties listing page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,14 @@
-import PropertyCard from '@/components/PropertyCard';
+import Properties from '@/components/Properties';
 import PropertySearchForm from '@/components/PropertySearchForm';
-import { fetchProperties } from '@/utils/requests';
+// import { fetchProperties } from '@/utils/requests';
 
 // import properties from '@/properties.json';
 
 const PropertiesList = async () => {
-  const properties = await fetchProperties();
+  // const properties = await fetchProperties();
 
-  // Sort properties by date
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  // // Sort properties by date
+  // properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
     <>
@@ -17,19 +17,7 @@ const PropertiesList = async () => {
           <PropertySearchForm />
         </div>
       </section>
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
